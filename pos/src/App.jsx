fdec884d61aeae5ccb52c6068e6bbd8f3ce974ce
Jay Parmar <53110999+jay-s-p@users.noreply.github.com>
2023-02-16 13:55:35 +0530

    Major change in POS

--- pos/src/App.jsx
+++ pos/src/App.jsx
@@ -1,110 +1,138 @@
 import "./App.css";
 import grocery from "./grocery.json";
+// import grocery from "./fruits.json";
 import "./temp.png";
+import Header from './Header'
 import ProductCard from './ProductCard'
 import CartItem from './CartItem'
 import React, { useState } from "react";
 
-const categories = grocery.map(item => item.category[0]).filter((item, i, arr) => arr.indexOf(item) == i)
+let categoriesSet = new Set();
+grocery.map(product => {
+  product.categories.map(categories => categoriesSet.add(categories));
+})
+const category = Array.from(categoriesSet);
+
+let searchItem = "";
 
 function App() {
 
-  const [cartItem, setCartItem] = useState([]);
-  const [category, setCategory] = useState("Fruits");
+  const [cartItem, setCartItem] = useState({});
+  const [categories, setCategories] = useState("all");
+  const [totalPrice, setTotalPrice] = useState(0)
 
-  const catProducts = grocery.filter(function (item) {
-    return item.category[0] == category
-  });
+  let catProducts = (categories == "all") ?
+    grocery : (categories.indexOf("search") != -1) ?
+      grocery.filter((item) => {
+        return (item.categories[0].toLowerCase().indexOf(searchItem.toLowerCase()) != -1) || (item.product_name.toLowerCase().indexOf(searchItem.toLowerCase()) != -1)
+      }) :
+      grocery.filter((item) => item.categories[0] == categories);
 
   return (
-    <div className="App container-fluid container-xl d-flex flex-column">
-      <div className="d-grid" style={{ height: "10vh" }}>
-        <h1 className="m-auto">Grocery shop</h1>
-      </div>
-      <div className="row flex-grow-1 ">
+    <>
+      <Header />
+      <div className="App container-fluid container-xxl d-flex flex-column">
+        <div className="row flex-grow-1">
+          <div className="col-lg-8 pt-3 px-0 pe-3 ps-3" style={{ height: "90vh" }}>
+            <div className="card bg-transparent border-0" style={{ height: "100%" }}>
+              <div className="card-body overflow-auto d-flex flex-column scrollbar-style-1 rounded-3 pb-0" >
+                {/* Search bar */}
+                <div className=" mx-auto mb-3 mt-3" style={{ width: "100%" }}>
+                  <input
+                    onChange={(event) => {
+                      if (event.target.value == "") {
+                        document.getElementById("catItemAll").checked = true;
+                        return;
+                      }
+                      var ele = document.getElementsByName("btn-radio");
+                      // Setting all categories unselectd
+                      for (var i = 0; i < ele.length; i++)
+                        ele[i].checked = false
 
-        <div className="col-lg-8" style={{ height: "90vh" }}>
-          <div className="card pb-3" style={{ height: "96%" }}>
-            <div className="card-body overflow-auto d-flex flex-column scrollbar-style-1">
-              <div className="mb-3 mt-3">
-                <input type="text" className="form-control" id="product" placeholder="Enter Product Name" name="product" />
-              </div>
-              <div className="btn-group" role="group" aria-label="Basic checkbox toggle button group">
-                {
-                  categories.map((item, index) => {
-                    return (
-                      <>
-                        <input type="radio" className="btn-check" name="btn-radio" id={"catItem" + index} autoComplete="off" />
-                        <label className="btn btn-outline-dark" htmlFor={"catItem" + index} onClick={() => setCategory(item)}>{item}</label>
-                      </>
-                    )
-                  })
-                }
-              </div>
-              <div className="row flex-grow-1">
-                {catProducts.map((item, index) => {
-                  return (
-                    <ProductCard setCartItem={setCartItem} item={item} />
-                  )
-                })}
+                      searchItem = event.target.value;
+                      setCategories("search" + searchItem)
+                    }}
+                    type="text" className="form-control search-bar bg-transparent" id="product" placeholder="Enter Product Name" name="product" />
+
+                </div>
+                {/* Category bar */}
+                <div className="d-flex flex-shrink-0 gap-3 " id="exp-2" style={{ height: "4em", alignItems: "center" }}>
+                  {/* <div style={{ height: "50%", width: "10px", margin: "auto 0", flexShrink: 0 }}></div> */}
+                  <input type="radio" className="btn-check" name="btn-radio" id="catItemAll" autoComplete="off" defaultChecked />
+                  <label className="btn btn-outline-dark" htmlFor="catItemAll" onClick={() => setCategories("all")}>All</label>
+                  <div style={{ height: "50%", width: "2px", background: "#aaa", margin: "auto 0", flexShrink: 0 }}></div>
+                  <div className="d-flex gap-3 overflow-auto  scrollbar-style-0">
+                    {
+                      category.map((item) => {
+                        // console.log(item);
+                        return (
+                          <>
+                            {/* <div className="btn-group" role="group" aria-label="Basic checkbox toggle button group"> */}
+                            <input type="radio" className="btn-check" name="btn-radio" id={"catItem" + item} autoComplete="off" />
+                            <label className="btn btn-outline-dark " htmlFor={"catItem" + item} onClick={() => setCategories(item)}>{item}</label>
+                            {/* </div> */}
+                          </>
+                        )
+                      })
+                    }
+                  </div>
+                  <div style={{ height: "50%", width: "10px", margin: "auto 0", flexShrink: 0 }}></div>
+                </div>
+                {/* Products */}
+                <div id="exp-1" className="d-flex justify-content-evenly flex-wrap overflow-auto scrollbar-style-0 px-1 rounded-4 pb-3">
+                  {catProducts.map(item =>
+                    <ProductCard item={item} setTotalPrice={setTotalPrice} setCartItem={setCartItem} />
+                  )}
+                </div>
               </div>
             </div>
           </div>
-        </div>
 
-        <div className="col-lg-4" style={{ height: "90vh" }}>
-          <div className="card" style={{ height: "96%" }}>
-            <div className="card-body h-100 d-flex flex-column">
-              <div className="mb-3 mt-3">
-                <input type="text" className="form-control" id="customer" placeholder="Enter Customer Name" name="customer" />
-              </div>
+          <div className="col-lg-4 py-3 px-0 pe-4" style={{ height: "90vh" }}>
+            <div className="card rounded-4" style={{ height: "100%", zIndex: "0" }}>
+              <div className="card-body h-100 d-flex flex-column">
+                <div className="mb-3 mt-3">
+                  <input type="text" className="form-control" id="customer" placeholder="Enter Customer Name" name="customer" />
+                </div>
 
-              <div class="table-responsive flex-grow-1 scrollbar-style-0 mb-3">
-                <table className="table table-hover table-borderless  text-start rounded-table-row">
-                  <thead>
-                    <tr>
-                      <th style={{ width: "60%" }}>Products</th>
-                      {/* <th style={{ width: "10%" }}>Qty</th> */}
-                      <th className="text-center" style={{ width: "20%" }}>Price</th>
-                      <th style={{ width: "10%" }}>Sub&nbsp;Total</th>
-                    </tr>
-                  </thead>
-                  <tbody className="">
-                    {/* <CartItem /> */}
-                    {
-                      cartItem.map((item) => {
-                        // console.log(cartItem);
-                        // if (item != undefined)
-                        return <CartItem setCartItem={setCartItem} item={item} />
+                <div id="exp-3" className="table-responsive flex-grow-1 scrollbar-style-0 mb-3">
+                  {
+                    (Object.keys(cartItem).length == 0) ?
+                      <div style={{ height: "100%", overflow: "hidden" }}>
+                        <img src="https://shop.millenniumbooksource.com/static/images/cart1.png" alt="Cart is empty"
+                          style={{ width: "100%", height: "100%", scale: "2", objectFit: "contain" }} />
+                      </div>
+                      :
+                      <table className="table table-hover table-borderless  text-start rounded-table-row">
+                        <thead>
+                          <tr>
+                            {/* <th className="p-0"></th> */}
+                            <th style={{ width: "60%" }}>Products</th>
+                            {/* <th style={{ width: "10%" }}>Qty</th> */}
+                            <th className="text-center" style={{ width: "20%" }}>Price</th>
+                            <th style={{ width: "10%" }}>Sub&nbsp;Total</th>
+                          </tr>
+                        </thead>
+                        <tbody className="">
+                          {
+                            Object.entries(cartItem).map((item) =>
+                              <CartItem setCartItem={setCartItem} setTotalPrice={setTotalPrice} item={item[1]} />
+                            )
+                          }
+                        </tbody>
+                      </table>
+                  }
+                </div>
 
-                      })
-                    }
-                    {/* {
-                      (Object.entries(cartItem)).map((x) => {
-                        console.log(x[1]);
-                        return (
-                          <CartItem setCartItem={setCartItem} item={x[1]} />
-                        )
-                      })} */}
-                    {/* {Object.keys(cartItem).map((item) => {
-                      // console.log(cartItem[item])
-                      return(
-                        <CartItem setCartItem={setCartItem} item={cartItem[item]}/>
-                      )
-                    })} */}
-                  </tbody>
-                </table>
-              </div>
-
-              <div style={{ textAlign: "right" }}>
-                <div class="table-responsive">
-                  <table class="table table-bordered">
-                    <tbody>
-                      <tr>
+                <div style={{ textAlign: "right" }}>
+                  <div className="table-responsive">
+                    <table className="table table-borderless">
+                      <tbody>
+                        {/* <tr>
                         <td className="text-start">Tax</td>
                         <th>00.00</th>
-                      </tr>
-                      {/* <tr>
+                      </tr> */}
+                        {/* <tr>
                         <td className="text-start">Discount</td>
                         <th>00.00</th>
                       </tr>
@@ -112,23 +140,24 @@ function App() {
                         <td className="text-start">Shipping cost</td>
                         <th>00.00</th>
                       </tr> */}
-                      <tr>
-                        <td className="text-start">Total</td>
-                        <th>00.00</th>
-                      </tr>
-                    </tbody>
-                  </table>
+                        <tr>
+                          <td className="text-start">Total</td>
+                          <th>₹{totalPrice}</th>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
                 </div>
-              </div>
-              <div style={{ textAlign: "right" }}>
-                <button className="btn btn-outline-danger">Reset</button>
-                <button className="btn btn-success mx-2">Pay</button>
+                <div className="d-flex mb-2 mx-2">
+                  <button style={{ flexGrow: "1" }} className="btn btn-outline-dark" onClick={() => window.location.reload(true)}>Reset</button>
+                  <button style={{ flexGrow: "3" }} className="btn btn-success ms-3">Pay</button>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
+      </div >
+    </>
   );
 }
 
--- pos/src/ProductCard.jsx
+++ pos/src/ProductCard.jsx
@@ -5,28 +5,21 @@ export default function ProductCard(props) {
     <div className="card-style-1 card rounded-4 m-3 "
       onClick={() => {
         props.setCartItem((cartItem) => {
-          //   ++cartItem[props.item.product_id]["quantity"]
-          // else
           if (cartItem["product_"+props.item.product_id])
             return cartItem;
-          let x = 0
           cartItem["product_"+props.item.product_id] = {
+            "image_url": props.item.image_url,
             "product_id": props.item.product_id,
             "quantity": 1,
             "product_name": props.item.product_name,
             "price": props.item.price
           }
-          // console.log(cartItem)
           props.setTotalPrice((totalPrice) => totalPrice + props.item.price)
           return cartItem;
-
         })
-        props.setCartItem(ct => ({ ...ct }))
-      }}
-    >
-      {
-        // console.log(props.item.image_url)
-      }
+        // props.setCartItem(ct => ({ ...ct }))
+        // console.log("i");
+      }}>
       <img className="card-img-top p-3 rounded-4 card-img-w-h"
         alt="Title"
         src={(props.item.image_url) ? props.item.image_url : "https://images.pexels.com/photos/6985001/pexels-photo-6985001.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}
